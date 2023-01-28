5ddf7ea69fe20f6cf2c71cf945fd1e4f756dba3b
JDRGit <jaimerodriguez85@gmail.com>
2023-01-28 12:51:43 -0500

    initial commit

---
+++ src/components/Student.js
@@ -0,0 +1,19 @@
+import React from "react";
+import Score from "./Score";
+
+const Student = ({ student }) => {
+  return (
+    <div>
+      <h2>{student.name}</h2>
+      <p>{student.bio}</p>
+      <h3>Scores:</h3>
+      <div>
+        {student.scores.map((score, index) => (
+          <Score key={index} score={score} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Student;
